fa7c5f7b78f0e14907826144acdec25d71d95cec
Sowmma Roy <roysowmma@gmail.com>
2023-04-07 15:16:11 -0400

    Established and implemented Redux store

--- src/App.js
+++ src/App.js
@@ -11,30 +11,35 @@ import Clients from "./components/Clients";
 import Contact from "./components/Contact";
 import Menu from "./components/Menu";
 
+import store from './redux/store.js';
+import { Provider } from 'react-redux'
+
+
 
 function App() {
   return (
+
     <div className="App">
+      <Provider store={store}>
+        <Container maxWidth="sm" sx={{ marginTop:"50px"}}>
 
-    <Container maxWidth="sm" sx={{ marginTop:"50px"}}>
+          <BrowserRouter>
+            <Box sx={{ mb:3}}>
+              <Menu />  
+            </Box>
 
-      <BrowserRouter>
-        <Box sx={{ mb:3}}>
-          <Menu />  
-        </Box>
+            <Routes>{/* section dedicated to displaying the URL user is in */}
+              <Route exact path="about" element={<About />}></Route>{/*in this path display this element which in this is a component called About */}
 
-        <Routes>{/* section dedicated to displaying the URL user is in */}
-          <Route exact path="about" element={<About />}></Route>{/*in this path display this element which in this is a component called About */}
+              <Route exact path="clients" element={<Clients />}></Route>
 
-          <Route exact path="clients" element={<Clients />}></Route>
+              <Route exact path="contact" element={<Contact />}></Route>
 
-          <Route exact path="contact" element={<Contact />}></Route>
+            </Routes>
+          </BrowserRouter>
 
-        </Routes>
-      </BrowserRouter>
-
-    </Container>
-
+        </Container>
+      </Provider>
     </div>
   );
 }
